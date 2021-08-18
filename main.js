78e6673b16233835491537b3cabcc85da9061edf
labeastone1 <87197782+labeastone1@users.noreply.github.com>
2021-08-18 18:37:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function  preaload(){}
+
+
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+}
+
+
+
+function draw(){
+ image(video,0,0,300,300);
+}
